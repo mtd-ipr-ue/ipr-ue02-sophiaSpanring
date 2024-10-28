17d58c5cb32b89f154271eb559521007fd823236
Sophia Spanring <s2410238052@students.fh-hagenberg.at>
2024-10-28 17:02:40 +0100

    first commit

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,24 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.println("Enter three numbers and press Enter after each: ");
+
+        int numberOne = In.readInt();
+        int numberTwo = In.readInt();
+        int numberThree = In.readInt();
+
+        int max = 0;
+
+        if (numberOne >= numberTwo && numberOne >= numberThree) {
+            max = numberOne;
+        } else if (numberTwo > numberOne && numberTwo >= numberThree) {
+            max = numberTwo;
+        } else {
+            max = numberThree;
+        }
+
+        Out.println("The max number is " + max);
     }
 }
